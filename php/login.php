b0c5128965f3d5e76f4d2070c2042afce94d74ac
Aranza Laguna <aranzzita@gmail.com>
2015-12-05 12:21:36 -0600

    login

--- php/login.php
+++ php/login.php
@@ -9,7 +9,7 @@ $error= "Correo o contraseña invalidos";
 
      $correo= $_POST['correo'];
      $contraseña = $_POST['contraseña'];
-     $con;
+     
      $tsql="SELECT * FROM usuarios WHERE correo = '$correo'  AND contraseña = '$contraseña'";
      $res= $con->prepare($tsql);
      $res->execute($params);
@@ -22,24 +22,7 @@ $error= "Correo o contraseña invalidos";
     header('Location: error.html');
     die(print_r( sqlsrv_errors(), true));
 }
-//$con = sqlsrv_connect($host, $connectinfo);
- if($con == true){
- echo "Conexión establecida";
- header('Location: registro.php');
- }else{
- echo "Error al conectar la base de datos\n";
-  die(print_r( sqlsrv_errors(), true));
- }
-    $nombreusuario = $_REQUEST['nombreusuario'];
-    $contraseña = $_REQUEST['contraseña'];
-    $consulta = "SELECT * FROM usuarios WHERE nombreusuario = 'nombreusuario'  AND contraseña = 'contraseña'";
-    $res = sqlsrv_query($con , $consulta);
-    if($res == true){
-    $_SESSION['valid_user'] = true;
-    $_SESSION['nombreusuario'] = $nombreusuario;
-    header('Location: intro-page.html');
-    die();
-}else{
-    header('Location: error.html');
-    die(print_r( sqlsrv_errors(), true));
 }
+ }
+
+ ?>
