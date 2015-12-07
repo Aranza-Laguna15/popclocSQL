1f33efd3b81c45e1bab5fc8d77e5de8842d2fc79
Aranza Laguna <aranzzita@gmail.com>
2015-12-07 14:32:12 -0600

    update session

--- php/session.php
+++ php/session.php
@@ -1,24 +1,20 @@
 <?php
-
     $username = "popcloc@b63ioz7h2m"; 
     $password = "Manuel_$%&"; 
     $host = "b63ioz7h2m.database.windows.net,1433"; 
     $dbname = "databasepopcloc";
-    try{
+ 
     $con = new PDO("sqlsrv:server=$host;Database=$dbname", $username, $password);
-     $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); 
 
-}catch( PDOException $e ) {
-print( "Error connecting to SQL Server.\n" );
-die(print_r($e->getMessage()));
-}
 session_start();
 $user_check=$_SESSION['login_user'];
-$res=$con -> prepare ("SELECT * FROM usuarios WHERE correo= :user_check");
+$res=$con -> prepare ("SELECT correo FROM usuarios WHERE correo= :user_check");
 $res -> bindParam(':user_check',$user_check);
 $res->execute();
 $rows = $res->fetch(PDO::FETCH_ASSOC);
-$login_session=$row['correo'];
+$login_session=$rows['correo'];
+$user_id=$rows['id'];
+$contrasena=$rows['contrasena'];
 if(!isset($login_session))
         {
             $con = null; 
