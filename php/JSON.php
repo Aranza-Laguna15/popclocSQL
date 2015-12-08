4c443948220aa3a89f10c139c1dfceae72af8bdd
Aranza Laguna <aranzzita@gmail.com>
2015-12-08 12:59:18 -0600

    update JSON.php

--- php/JSON.php
+++ php/JSON.php
@@ -1,4 +1,5 @@
 <?php
+include('login.php');
 $username = "popcloc@b63ioz7h2m"; 
     $password = "Manuel_$%&"; 
     $host = "b63ioz7h2m.database.windows.net,1433"; 
@@ -9,10 +10,23 @@ $username = "popcloc@b63ioz7h2m";
 }catch( PDOException $e ) {
 print( "Error connecting to SQL Server.\n" );
 die(print_r($e->getMessage()));
+$response["success"] = 0;
+        $response["message"] = "Problema con la base de datos, vuelve a intetarlo";
+        die(json_encode($response));
 }
 $res = $con ->query("SELECT * FROM usuarios");
 foreach($res as $registro){
      $users= array("id"=>$registro[0], "claveusuario"=>$registro[1],"nombreusuario"=>$registro[2], "correo"=>$registro[3], "contrasena"=>$registro[4], "sexo"=>$registro[5], "edad"=>$registro[6]);
     print_r(json_encode($users)); 
 }
+if( $rows > 0){
+    $response["success"] = 1;
+        $response["message"] = "Login correcto!";
+        die(json_encode($response));
+    } else {
+        $response["success"] = 0;
+        $response["message"] = "Login INCORRECTO";
+        die(json_encode($response));
+    }
+
 ?>
