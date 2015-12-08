2f632c31b0f9fbf95bf3f77bfa23e20c4a4fb5b1
Aranza Laguna <aranzzita@gmail.com>
2015-12-08 15:58:19 -0600

    update registrer.php

--- php/registrer.php
+++ php/registrer.php
@@ -8,21 +8,24 @@
     $con = new PDO("sqlsrv:server=$host;Database=$dbname", $username, $password);
  
 session_start();
-
-$clave='PopClocUser';
 $query="INSERT INTO usuarios (claveusuario,nombreusuario,correo,contrasena,sexo,edad) VALUES (:claveusuario, :nombreusuario, :correo, :contrasena, :sexo, :edad)";
 
-if(isset($_POST['submit'])){
-    try{
-  $res=$con -> prepare ($query);
-  $res -> bindParam(':claveusuario',$clave,PDO::PARAM_STR);  
+$clave='PopClocUser';
+$users_array=array(':claveusuario'=>$clave, ':nombreusuario'=>$_POST['nombre'],
+':correo'=>$_POST['correo'],':contrasena'=>$_POST['contrasena'],':sexo'=>$_POST['sexo'],
+':edad'=>$_POST['edad']);
+
+ /*$res -> bindParam(':claveusuario',$clave,PDO::PARAM_STR);  
   $res -> bindParam(':nombreusuario',$_POST['nombre'],PDO::PARAM_STR);  
   $res -> bindParam(':correo',$_POST['correo'],PDO::PARAM_STR);  
   $res -> bindParam(':contrasena',$_POST['contrasena'],PDO::PARAM_STR);  
   $res -> bindParam(':sexo',$_POST['sexo'],PDO::PARAM_STR);  
-  $res -> bindParam(':edad',$_POST['edad'],PDO::PARAM_INT);
-  $exec=$res -> execute();
-  $res -> execute();
+  $res -> bindParam(':edad',$_POST['edad'],PDO::PARAM_INT);*/
+if(isset($_POST['submit'])){
+    try{
+  $stmt=$con -> prepare ($query);
+  $result=$stmt -> execute($users_array);
+  
   header('Location: error.php'); 
     }catch (PDOException $ex) {
     print( "Error al insertar usuario: " );
