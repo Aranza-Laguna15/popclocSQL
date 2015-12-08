9f518062d5aa3dd46b30d684523612dfb88d2aa0
Aranza Laguna <aranzzita@gmail.com>
2015-12-08 02:00:54 -0600

    update registrer.php

--- php/registrer.php
+++ php/registrer.php
@@ -12,7 +12,7 @@ $correo=$_POST['correo'];
 $sexo=$_POST['sexo'];
 $edad=$_POST['edad'];
 $contrasena=$_POST['contrasena'];
-$clave="PopClocUser"+srand(time());
+$clave="PopClocUser"+rand(1000);
 
   $res=$con -> prepare ("INSERT INTO usuarios (claveusuario,nombreusuario,correo,contrasena,sexo,edad) VALUES (:clave , :nombreusuario , :correo , :contrasena , :sexo , :edad)");
   $res -> bindParam(':claveusuario',$clave,PDO::PARAM_STR);  
