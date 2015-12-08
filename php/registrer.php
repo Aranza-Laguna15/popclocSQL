d1a68937bd60c5d047fbfa62b5461fc232bc5870
Aranza Laguna <aranzzita@gmail.com>
2015-12-08 02:35:13 -0600

    update registro.php

--- php/registrer.php
+++ php/registrer.php
@@ -1,4 +1,5 @@
 <?php
+
     $username = "popcloc@b63ioz7h2m"; 
     $password = "Manuel_$%&"; 
     $host = "b63ioz7h2m.database.windows.net,1433"; 
@@ -6,15 +7,14 @@
     
     $con = new PDO("sqlsrv:server=$host;Database=$dbname", $username, $password);
  
-session_start();
 $nombreusuario=$_POST['nombre'];
 $correo=$_POST['correo'];
 $sexo=$_POST['sexo'];
 $edad=$_POST['edad'];
 $contrasena=$_POST['contrasena'];
-
+$clave="PopClocUser";
 if(isset($_POST['submit'])){
-    $clave="PopClocUser".$nombreusuario;
+    
   $res=$con -> prepare ("INSERT INTO usuarios (claveusuario,nombreusuario,correo,contrasena,sexo,edad) VALUES (:clave, :nombreusuario, :correo, :contrasena, :sexo, :edad)");
   $res -> bindParam(':claveusuario',$clave,PDO::PARAM_STR);  
   $res -> bindParam(':nombreusuario',$nombreusuario,PDO::PARAM_STR);  
@@ -22,7 +22,7 @@ if(isset($_POST['submit'])){
   $res -> bindParam(':contrasena',$contrasena,PDO::PARAM_STR);  
   $res -> bindParam(':sexo',$sexo,PDO::PARAM_STR);  
   $res -> bindParam(':edad',$edad,PDO::PARAM_INT);
-  $res->execute();
+  $res -> execute();
   header('Location: ../index.php'); 
 }
 ?>
