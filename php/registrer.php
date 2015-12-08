46dbdf6367c8ecd5ee6bc57752f7e3a36f0f3ccc
Aranza Laguna <aranzzita@gmail.com>
2015-12-08 01:41:08 -0600

    update registrer.php

--- php/registrer.php
+++ php/registrer.php
@@ -3,8 +3,13 @@
     $password = "Manuel_$%&"; 
     $host = "b63ioz7h2m.database.windows.net,1433"; 
     $dbname = "databasepopcloc";
+    try{
     $con = new PDO("sqlsrv:server=$host;Database=$dbname", $username, $password);
-    
+     $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); 
+}catch( PDOException $e ) {
+print( "Error connecting to SQL Server.\n" );
+die(print_r($e->getMessage()));
+}
 session_start();
 $nombreusuario=$_POST['nombre'];
 $correo=$_POST['correo'];
@@ -14,14 +19,15 @@ $contrasena=$_POST['contrasena'];
 $clave="PopClocUser"+srand(time());;
 if(isset($_POST['submit'])){
 try{
-  $res=$con -> prepare ("INSERT INTO usuarios (claveusuario,nombreusuario,correo,contrasena,sexo,edad) VALUES ('$clave' , '$nombreusuario' , '$correo' , '$contrasena' , '$sexo' , '$edad')");
+  $res=$con -> prepare ("INSERT INTO usuarios (claveusuario,nombreusuario,correo,contrasena,sexo,edad) VALUES (:clave , :nombreusuario , :correo , :contrasena , :sexo , :edad)");
   /*$res -> bindParam(':claveusuario',$clave);  
   $res -> bindParam(':nombreusuario',$nombreusuario);  
   $res -> bindParam(':correo',$correo);  
   $res -> bindParam(':contrasena',$contrasena);  
   $res -> bindParam(':sexo',$sexo);  
-  $res -> bindParam(':edad',$edad);*/
-  $rows=$res->execute();
+  $res -> bindParam(':edad',$edad,PDO::PARAM_INT);*/
+  $rows=$res->execute(array(':claveusuario'=>$clave, ':nombreusuario'=>$nombreusuario,
+  ':correo'=>$correo, ':contrasena'=>$contrasena, ':sexo'=>$sexo,':edad'=>$edad));
   if($rows == 1){
      alert('Usuario registrado, inicia sesión');
   header('Location: ../index.php'); 
