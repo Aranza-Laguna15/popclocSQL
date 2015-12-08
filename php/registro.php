02ee0c15b429a2e35089c9d19409d670d1b93238
Aranza Laguna <aranzzita@gmail.com>
2015-12-08 00:02:21 -0600

    update registro.php

--- php/registro.php
+++ php/registro.php
@@ -55,13 +55,13 @@
         <div class="container">
             <div class="row">
                 <h2>¡Registrate ahora!</h2>
-                <form role="form">
+                <form role="form" action="registrer.php" method="post">
                     <div class="col-lg-6">
                         <br>
                         <div class="form-group">
                             <label for="InputName">Nombre</label>
                             <div class="input-group">
-                                <input type="text" class="form-control" name="nombreusuario" placeholder="Nombre" required>
+                                <input type="text" class="form-control" name="nombre" placeholder="Nombre" required>
                                 <span class="input-group-addon"><span class=""></span></span>
                             </div>
                         </div>
@@ -75,25 +75,25 @@
                         <div class="form-group">
                             <label for="InputPassword">Contraseña</label>
                             <div class="input-group">
-                                <input type="password" class="form-control" name="contraseña" placeholder="Contraseña" required>
+                                <input type="password" class="form-control" name="contrasena" placeholder="Contraseña" required>
                                 <span class="input-group-addon"><span class=""></span></span>
                             </div>
                         </div>
                         <div class="form-group">
                             <label for="InputAge">Edad</label>
                             <div class="input-group">
-                                <select class="form-control">
-                                    <option>12</option><option>13</option><option>14</option><option>15</option>
-                                    <option>16</option><option>17</option><option>18</option><option>19</option>
-                                    <option>20</option><option>21</option><option>22</option><option>23</option>
-                                    <option>24</option><option>25</option><option>26</option><option>27</option>
-                                    <option>28</option><option>29</option><option>30</option><option>31</option>
-                                    <option>32</option><option>33</option><option>34</option><option>35</option>
-                                    <option>36</option><option>37</option><option>38</option><option>39</option>
-                                    <option>40</option><option>41</option><option>42</option><option>43</option>
-                                    <option>44</option><option>45</option><option>46</option><option>47</option>
-                                    <option>48</option><option>49</option><option>50</option><option>51</option>
-                                    <option>52</option><option>53</option><option>54</option><option>55</option>
+                                <select class="form-control" name="edad">
+                                    <option value="12">12</option><option value="13">13</option><option value="14">14</option><option value="15">15</option>
+                                    <option value="16">16</option><option value="17">17</option><option value="18">18</option><option value="19">19</option>
+                                    <option value="20">20</option><option value="21">21</option><option value="22">22</option><option value="23">23</option>
+                                    <option value="24">24</option><option value="25">25</option><option value="26">26</option><option value="27">27</option>
+                                    <option value="28">28</option><option value="29">29</option><option value="30">30</option><option value="31">31</option>
+                                    <option value="32">32</option><option value="33">33</option><option value="34">34</option><option value="35">35</option>
+                                    <option value="36">36</option><option value="37">37</option><option value="38">38</option><option value="39">39</option>
+                                    <option value="40">40</option><option value="41">41</option><option value="42">42</option><option value="43">43</option>
+                                    <option value="44">44</option><option value="45">45</option><option value="46">46</option><option value="47">47</option>
+                                    <option value="48">48</option><option value="49">49</option><option value="50">50</option><option value="51">51</option>
+                                    <option value="52">52</option><option value="53">53</option><option value="54">54</option><option value="55">55</option>
                                 </select>
                             </div>
                         </div>
@@ -101,22 +101,16 @@
                         <label for="InputSex">Sexo</label>
                         <div class="radio">
                         <label>
-                            <input type="radio" name="sexo" id="optionsRadios1" value="mujer" >
+                            <input type="radio" name="sexo" id="optionsRadios1" value="Mujer" >
                             Mujer
                         </label>
                         </div>
                         <div class="radio">
                         <label>
-                            <input type="radio" name="sexo" id="optionsRadios2" value="hombre">
+                            <input type="radio" name="sexo" id="optionsRadios2" value="Hombre">
                             Hombre
                         </label>
                         </div>
-                        <div class="radio">
-                        <label>
-                            <input type="radio" name="sexo" id="optionsRadios3" value="otro">
-                            Otro
-                        </label>
-                        </div
                         </div>
                         <input type="submit" name="submit" id="submit" value="Enviar" class="btn btn-enviar pull-right">
                     </div>
