c273c5a32e54e3d4e4d54f60805de343adca0ec8
Aranza Laguna <aranzzita@gmail.com>
2015-12-09 00:01:55 -0600

    update registrer.php

--- php/registrer.php
+++ php/registrer.php
@@ -5,8 +5,6 @@
     $dbname = "databasepopcloc";
     
 if(isset($_POST['submit'])){
-    //$sexo=$_POST['sexo'];
-    //$edad=$_POST['edad'];
     $claveusuario='PopClocUser';
     try{
         $con = new PDO("sqlsrv:server=$host;Database=$dbname", $username, $password);
@@ -20,23 +18,13 @@ if(isset($_POST['submit'])){
         $consulta -> bindParam(':edad',$_POST['edad']);
         $consulta -> bindParam(':sexo',$_POST['sexo']);  
         $consulta -> execute();
-       /* $newID=$con->lastInsertId();
-        $newID++;*/
-        
-       header('Location: error.php');  
+       
+       header('Location: ../index.php');  
    
 }catch(PDOException $e)
     {
     echo $sql . "<br>" . $e->getMessage();
     }
 }
-
- /*  $sql -> bindParam(':claveusuario',$claveusuario,PDO::PARAM_STR);  
-    $sql -> bindParam(':nombreusuario',$_POST['nombre'],PDO::PARAM_STR);  (:claveusuario, :nombreusuario, :correo, :contrasena, :sexo, :edad)
-    $sql -> bindParam(':correo',$_POST['correo'],PDO::PARAM_STR);  
-    $sql -> bindParam(':contrasena',$_POST['contrasena'],PDO::PARAM_STR);  
-    $sql -> bindParam(':sexo',$_POST['sexo'],PDO::PARAM_STR);  
-    $sql -> bindParam(':edad',$_POST['edad'],PDO::PARAM_STR);*/
-// $users_array=array(':claveusuario'=>$clave, ':nombreusuario'=>$_POST['nombre'], ':correo'=>$_POST['correo'],':contrasena'=>$_POST['contrasena'],':sexo'=>$_POST['sexo'], ':edad'=>$_POST['edad']);
 
 ?>
